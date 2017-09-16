6359863193243de9d99223c8109c408db87954b8
Hiswe <hiswehalya@gmail.com>
2017-09-16 19:46:18 +0200

    BUILD – move to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,158 +1,58 @@
-'use strict';
+'use strict'
 
-var gulp          = require('gulp');
-var $             = require('gulp-load-plugins')();
-var browserSync   = require('browser-sync');
-var run           = require('run-sequence');
-var args          = require('yargs').argv;
+const gulp                  = require( `gulp` )
+const $                     = require( `gulp-load-plugins` )()
+const browserSync           = require( `browser-sync` ).create()
+const { reload }            = browserSync
+const nodemon               = require( `nodemon` )
+const webpack               = require( `webpack` )
+const args                  = require( `yargs` ).argv
 
-var reload        = browserSync.reload
-var isDev         = args.prod !== true
-var jsBasedir     = __dirname + '/js'
+const isDev       = args.prod !== true
+const jsBasedir   = __dirname + '/js'
+const bundler     = webpack( require(`./webpack.config.js`) )
 
-
-function onError(err) {
-  $.util.beep();
-  if (err.annotated)      { $.util.log(err.annotated); }
-  else if (err.message)   { $.util.log(err.message); }
-  else                    { $.util.log(err); }
-  return this.emit('end');
+const onError = err => {
+  $.util.beep()
+  if (err.annotated)      { $.util.log(err.annotated) }
+  else if (err.message)   { $.util.log(err.message) }
+  else                    { $.util.log(err) }
+  return this.emit('end')
 }
 
 ////////
 // JS
 ////////
 
-var browserify    = require('browserify');
-var babelify      = require('babelify');
-var watchify      = require('watchify');
-var envify        = require('envify');
-var jadeify       = require('jadeify');
-var source        = require('vinyl-source-stream');
-var vinylBuffer   = require('vinyl-buffer');
-
-// https://github.com/vigetlabs/gulp-starter/issues/75#issuecomment-70672188
-
-//----- LIBRARIES
-
-var npmLibs       = [
-  // 'jquery',
-  // 'autosize',
-  // 'epiceditor/src/editor',
-  // 'marked',
-  'react',
-  'react-dom',
-  'react-router',
-  'lodash.omit',
-  'moment',
-  'marked',
-  'redux',
-  'react-redux',
-];
-
-gulp.task('js-lib', function () {
-  var b = browserify({
-    debug: true,
-    // need to parse for envify
-    // noParse: npmLibs,
-  });
-
-  npmLibs.forEach(function(lib) {
-    b.require(lib);
-  });
-
-  b.transform(envify({
-    _: 'purge',
-    NODE_ENV: isDev ? 'development' : 'production',
-  }))
-
-  return b
-    .bundle()
-    .pipe(source('concompte-lib.js'))
-    .pipe(gulp.dest('public'));
-});
-
-//----- FRONT APPLICATION
-
-gulp.task('js-app', function () {
-
-  var b = browserify({
-    cache:        {},
-    packageCache: {},
-    debug:        isDev,
-    extensions:   ['.js', '.jsx'],
-    entries:      ['./js/index.js']
-  })
-  .external(npmLibs)
-  .transform(babelify, {
-    presets: ['es2015', 'react'],
-    // plugins: ['transform-object-assign'],
-  })
-
-  // .transform(envify({
-  //   _: 'purge',
-  //   NODE_ENV: isDev ? 'development' : 'production',
-  //   LOG: isDev,
-  // }))
-
-
-  if (isDev) {
-    $.util.log('init watchify');
-    b = watchify(b);
-    b.on('update', function () {
-      $.util.log('update bundle');
-      bundleShare(b);
-    });
-  }
-
-  return bundleShare(b);
-
-});
-
-function bundleShare(b) {
-  return b
-    .bundle()
-    .on('error', function (err) {
-      console.log(err.message);
-      $.util.beep();
-      this.emit('end');
-    })
-    .pipe(source('concompte.js'))
-    .pipe(vinylBuffer())
-    // .pipe($.sourcemaps.init({loadMaps: true}))
-    // .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./public'))
-}
-
-gulp.task('js', ['js-lib', 'js-app']);
-
 ////////
 // CSS
 ////////
 
-var autoprefixer  = require('autoprefixer');
+const autoprefixer  = require( 'autoprefixer' )
 
-gulp.task('css', function () {
-  return gulp.src('css/index.styl')
-    .pipe($.plumber({errorHandler: onError}))
-    .pipe($.sourcemaps.init())
-      .pipe($.stylus({
-        'include css': true,
-        compress: false,
-        define: {
-          isProd: false,
-        }
-      }))
-      .pipe($.postcss([
-        autoprefixer({
-          browsers: ['> 1%', 'IE 9'],
-        }),
-      ]))
-      .pipe($.rename('concompte.css'))
-      .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('public'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
-});
+const css = _ => {
+  return gulp
+  .src( 'css/index.styl' )
+  .pipe( $.plumber(onError) )
+  .pipe( $.sourcemaps.init() )
+  .pipe($.stylus({
+    'include css': true,
+    compress: false,
+    define: {
+      isProd: false,
+    }
+  }))
+  .pipe( $.postcss([
+    autoprefixer( {} ),
+  ]) )
+  .pipe( $.sourcemaps.write() )
+  .pipe( $.rename('concompte.css') )
+  .pipe( gulp.dest('public') )
+  .pipe( reload({stream: true}) )
+
+}
+
+css.description = `Build CSS`
 
 ////////
 // ASSETS
@@ -173,36 +73,54 @@ gulp.task('css', function () {
 // DEV
 ////////
 
-gulp.task('dev', ['js'], function () {
-  gulp.watch([
-    'js/**/*.js',
-    'shared/*.js',
-    'views/**/*.jade',
-  ],                                ['js-app', browserSync.reload]);
-  gulp.watch('css/**/*.styl',       ['css']);
-});
+const watch = _ => {
+  gulp.watch( `views/**/*.jade`, css )
+  gulp.watch( `css/**/*.styl`, css)
+  // isomorphic app doesn't need module hot reload
+  bundler.watch(  {}, (err, stats) => {
+    // console.log(stats);
+    if (!err) reload()
+  })
+}
 
-gulp.task('browser-sync', ['nodemon'], function () {
-  browserSync.init(null, {
-    proxy:      'http://localhost:3000',
+const bs = _ => {
+  return browserSync.init(null, {
+    proxy:      `http://localhost:3000`,
     open:       false,
     port:       7000,
     ghostMode:  false,
-  });
-});
+  })
+}
 
-var init = true;
-gulp.task('nodemon', ['dev'], function (cb) {
-  return $.nodemon({
-    script: 'index.js',
-    ext: 'js json jsx',
-    watch: ['server.js', 'index.js',  'shared/**/*', 'server/**/*', 'db/**/*', 'views/**/*'],
-    env:    { 'NODE_ENV': 'development' }
-  }).on('start', function () {
+const runServer = cb => {
+  return nodemon({
+    script: `index.js`,
+    ext:    `js json jsx`,
+    watch: [
+      'server.js',
+      'index.js',
+      'shared/**/*',
+      'server/**/*',
+      'db/**/*',
+      'views/**/*'
+    ],
+    env: {
+      'NODE_ENV': 'development' ,
+    }
+  }).on('start', _ => {
     // https://gist.github.com/sogko/b53d33d4f3b40d3b4b2e#comment-1457582
     if (init) {
-      init = false;
-      cb();
+      init = false
+      cb()
     }
-  });
-});
+  })
+}
+
+const dev = _ => {
+  runServer()
+  watch()
+  bs()
+}
+
+gulp.task( 'css', css )
+gulp.task( 'dev', dev )
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,55 @@
-const path    = require( 'path' )
-const webpack = require( 'webpack' )
+`use strict`
+
+const path    = require( `path` )
+const webpack = require( `webpack` )
+const args    = require( `yargs` ).argv
+
+const isDev   = args.prod !== true
+
+const entry   = {
+  app:      `./js/index.js`,
+}
+const output  = {
+  filename: `concompte.js`,
+  path:     path.resolve( __dirname, 'public' )
+}
+const plugins = [
+  // https://webpack.js.org/plugins/commons-chunk-plugin/#passing-the-minchunks-property-a-function
+  new webpack.optimize.CommonsChunkPlugin({
+    name:     `vendor`,
+    filename: `concompte-lib.js`,
+    minChunks: m => m.context && m.context.indexOf( `node_modules` ) !== -1
+  }),
+]
+
+if (isDev) {
+  // plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+const rules = [
+  {
+    test:     /\.jsx?$/,
+    include: [
+      path.resolve( __dirname, 'js' ),
+      path.resolve( __dirname, 'shared' ),
+      path.resolve( __dirname, 'views' ),
+    ],
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['es2015', 'react'],
+      },
+    },
+  },
+]
 
 module.exports = {
-  entry: {
-    app:      `./js/index.js`,
-  },
-  output: {
-    filename: `concompte.js`,
-    path:     path.resolve( __dirname, 'public' )
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    // https://webpack.js.org/plugins/commons-chunk-plugin/#passing-the-minchunks-property-a-function
-    new webpack.optimize.CommonsChunkPlugin({
-      name:     `vendor`,
-      filename: `concompte-lib.js`,
-      minChunks: m => m.context && m.context.indexOf( `node_modules` ) !== -1
-    }),
-  ],
+  entry,
+  output,
+  watch:    true,
+  devtool:  'inline-source-map',
+  plugins,
   module: {
-    rules: [
-      {
-        test:     /\.jsx?$/,
-        include: [
-          path.resolve( __dirname, 'js' ),
-          path.resolve( __dirname, 'shared' ),
-          path.resolve( __dirname, 'views' ),
-        ],
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015', 'react'],
-          },
-        },
-      },
-    ],
-  }
+    rules,
+  },
 }
