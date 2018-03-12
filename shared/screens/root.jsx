010fdf572a7a931dfab3e2e72a4e6bf26dc82677
Hiswe <hiswehalya@gmail.com>
2018-03-12 10:30:52 +0700

    REACT – add error boundary

--- shared/screens/root.jsx
+++ shared/screens/root.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import * as user from '../ducks/user'
 import MainNav from '../components/layout/main-nav.jsx'
 import Notifications from '../components/layout/notifications.jsx'
+import ErrorBoundary from '../components/error-boundary.jsx'
 
 class Layout extends Component {
 
@@ -25,9 +26,11 @@ class Layout extends Component {
       <div id="react-wrapper">
         <h1 className="main-logo">Concompte</h1>
         <MainNav />
-        {/* child routes won't render without this */}
-        {/* https://www.npmjs.com/package/react-router-config#renderroutesroutes-extraprops-- */}
-        { renderRoutes(route.routes) }
+        <ErrorBoundary>
+          {/* child routes won't render without this */}
+          {/* https://www.npmjs.com/package/react-router-config#renderroutesroutes-extraprops-- */}
+          { renderRoutes(route.routes) }
+        </ErrorBoundary>
         <Notifications />
       </div>
     )
