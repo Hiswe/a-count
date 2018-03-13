6948bcc6eca960b90f0c19246822ea0285b72784
Hiswe <hiswehalya@gmail.com>
2018-03-13 13:49:33 +0700

    REDUX – empty state on logout

--- shared/ducks/combined-reducers.js
+++ shared/ducks/combined-reducers.js
@@ -3,12 +3,21 @@ import { combineReducers } from 'redux'
 import customers from './customers'
 import quotations from './quotations'
 import notifications from './notifications'
-import user from './user'
+import user, { LOGOUT } from './user'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   customers,
   quotations,
   notifications,
   user,
 })
+
+// make a global reducer
+// • this will allow us to manipulate all the state for logout
+//   https://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store/35641992#35641992
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT) state = void 0
+  return appReducer(state, action)
+}
+
 export default rootReducer
