597da5a05f96d5c10c2362df89590577690913b7
Hiswe <hiswehalya@gmail.com>
2018-03-21 15:08:25 +0700

    PROFILE – continue integration

--- shared/components/users/form.pres.jsx
+++ shared/components/users/form.pres.jsx
@@ -53,9 +53,14 @@ export default function UserFormPres( props ) {
       className="form form--profile"
     >
       <input type="hidden" name="id" defaultValue={formData.id} />
+      <input type="hidden" name="defaultInvoice[id]" defaultValue={ defaultInvoice.id } />
+      <input type="hidden" name="defaultQuotation[id]" defaultValue={defaultQuotation.id} />
 
       <Main content={() => (<Fragment>
         {/* USER */}
+        <h3 className={`${BASE_CLASS}__title`}>
+          <span>From information</span>
+        </h3>
         <div className={`${BASE_CLASS}__user`}>
           <div className={`${BASE_CLASS}__user-example`}>
             <Party title="from" {...formData} />
@@ -73,12 +78,11 @@ export default function UserFormPres( props ) {
           </div>
         </div>
         {/* PRODUCT */}
-        <div  className={`${BASE_CLASS}__product`}>
-          <input
-            type="hidden"
-            name="defaultQuotation[id]"
-            defaultValue={defaultQuotation.id}
-          />
+        <h3 className={`${BASE_CLASS}__title`}>
+          <span>Default product information</span>
+        </h3>
+        <div className={`${BASE_CLASS}__product`}>
+
           <div className={`${BASE_CLASS}__product-form`}>
             <Field
               name="defaultProduct[description]"
@@ -126,17 +130,12 @@ export default function UserFormPres( props ) {
             </ProductTable>
           </div>
         </div>
-      </Fragment>)} />
-
-      <Main
-        content={() => (
-          <Fragment>
-            <Field
-              name="defaultQuotation[mentions]"
-              label="mentions"
-              value={ defaultQuotation.mentions }
-              type="textarea"
-            />
+        {/* REFERENCES */}
+        <h3 className={`${BASE_CLASS}__title`}>
+          <span>Reference Format</span>
+        </h3>
+        <div className={`${BASE_CLASS}__references`}>
+          <div>
             <Field
               name="defaultQuotation[prefix]"
               label="prefix"
@@ -150,46 +149,59 @@ export default function UserFormPres( props ) {
               min="0"
               step="1"
             />
+          </div>
+          <div>
+            <Field
+              name="defaultInvoice[prefix]"
+              label="prefix"
+              value={ defaultInvoice.prefix }
+            />
+            <Field
+              name="defaultInvoice[startAt]"
+              label="start at"
+              value={ defaultInvoice.startAt }
+              type="number"
+              min="0"
+              step="1"
+            />
+          </div>
+        </div>
+
+        {/* MENTIONS */}
+        <h3 className={`${BASE_CLASS}__title`}>
+          <span>Mentions</span>
+        </h3>
+        <div className={`${BASE_CLASS}__mentions`}>
+
+        </div>
+      </Fragment>)} />
+
+      <Main
+        content={() => (
+          <Fragment>
+            <Field
+              name="defaultQuotation[mentions]"
+              label="mentions"
+              value={ defaultQuotation.mentions }
+              type="textarea"
+            />
+
             <PaperSheet>
               <input type="hidden" name="defaultProduct[id]" defaultValue={defaultProduct.id} />
               <Reference {...fakeQuotationReference} />
-              <Party title="quotation from" {...formData} />
-              <Party title="quotation to" {...customerExample} />
-              <h3>Default Product</h3>
               <ProductTable
                 products={ [defaultProduct] }
                 tax={ defaultQuotation.tax }
                 currency={ defaultQuotation.currency }
               >
-                <ProductLine
-                  fieldPath="defaultProduct"
-                  product={ defaultProduct }
-                  currency={ defaultQuotation.currency }
-                />
               </ProductTable>
               <Markdown text={ defaultQuotation.mentions }/>
             </PaperSheet>
             <fieldset className="card">
               <h3 className="card__title">Default Invoice</h3>
               <div className="card__content">
-                <input
-                  type="hidden"
-                  name="defaultInvoice[id]"
-                  defaultValue={ defaultInvoice.id }
-                />
-                <Field
-                  name="defaultInvoice[prefix]"
-                  label="prefix"
-                  value={ defaultInvoice.prefix }
-                />
-                <Field
-                  name="defaultInvoice[startAt]"
-                  label="start at"
-                  value={ defaultInvoice.startAt }
-                  type="number"
-                  min="0"
-                  step="1"
-                />
+
+
                 <Field
                   name="defaultInvoice[mentions]"
                   label="mentions"
