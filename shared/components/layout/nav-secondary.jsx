f1570bfc01967f08dba11e5afc1bce5dff00a395
Hiswe <hiswehalya@gmail.com>
2018-03-22 16:28:44 +0700

    NAV-SECONDARY – make a sticky header

--- shared/components/layout/nav-secondary.jsx
+++ shared/components/layout/nav-secondary.jsx
@@ -1,21 +1,72 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 import './nav-secondary.scss'
 
 const BASE_CLASS = `nav-secondary`
 
-export default function NavSecondary( props ) {
-  const { title, children } = props
-  return (
-    <header className={ BASE_CLASS }>
-      { title && (
-        <h2 className={`${BASE_CLASS}__title`}>{title}</h2>
-      ) }
-      { children && (
-        <div className={`${BASE_CLASS}__actions`}>
-          { children }
+export default class NavSecondary extends PureComponent {
+
+  constructor( props ) {
+    super( props )
+    this.state = {
+      isStuck: false,
+    }
+    this.handleIntersection = this.handleIntersection.bind( this )
+  }
+
+  componentDidMount() {
+    this.observeIntersection()
+  }
+
+  componentWillUnmount() {
+    this.unobserveIntersection()
+  }
+
+  //----- EVENTS
+
+  handleIntersection( entries ) {
+    const sentinelEntry = entries[ 0 ]
+    const isStuck = sentinelEntry.intersectionRatio === 0
+    this.setState( prevState => {
+      return { isStuck }
+    })
+  }
+
+  //----- UTILS
+
+  observeIntersection() {
+    if ( !`IntersectionObserver` in window ) return
+    const { wrapper } = this
+    if ( !wrapper ) return
+    const sentinel = document.createElement( `sentinel` )
+    sentinel.classList.add( `${BASE_CLASS}__sentinel` )
+    wrapper.insertBefore( sentinel, wrapper.firstChild )
+    this.observer = new IntersectionObserver( this.handleIntersection )
+    this.observer.observe( sentinel )
+  }
+
+  unobserveIntersection() {
+    this.observer.disconnect()
+  }
+
+  render() {
+    const { title, children } = this.props
+    const { isStuck } = this.state
+    const stickyClass = [ `${BASE_CLASS}__sticky` ]
+    if ( isStuck ) stickyClass.push( `${ stickyClass[ 0 ] }--is-stuck` )
+    return (
+      <header className={ BASE_CLASS } ref={ el => this.wrapper = el } >
+        <div className={ stickyClass.join(' ') }>
+          { title && (
+            <h2 className={`${BASE_CLASS}__title`}>{title}</h2>
+          ) }
+          { children && (
+            <div className={`${BASE_CLASS}__actions`}>
+              { children }
+            </div>
+          )}
         </div>
-      )}
-    </header>
-  )
+      </header>
+    )
+  }
 }
--- shared/components/ui/buttons.jsx
+++ shared/components/ui/buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './buttons.scss'
-const BASE_CLASS = `btn`
+const BASE_CLASS = `button`
 
 export function Button( props ) {
   const { className, to, ...others } = props
