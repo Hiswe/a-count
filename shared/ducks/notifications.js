8a8b1f0dc4e93e072321023ea45396a398989ce8
Hiswe <hiswehalya@gmail.com>
2018-03-23 13:29:18 +0700

    NOTIFICATIONS – better handling of errors

--- shared/ducks/notifications.js
+++ shared/ducks/notifications.js
@@ -14,13 +14,17 @@ import {
 const NAME = `notifications`
 export const REMOVE  = `@concompte/${NAME}/remove`
 export const ALL_POST = createActionNames( `[_a-zA-Z0-9]+`, `post`, `[_a-zA-Z0-9]+` )
-
+const postErrorRegexp = new RegExp( `^${ ALL_POST.ERROR }$` )
 
 const initialState = crio( [] )
 
-
 export default function reducer( state = initialState, action ) {
-  const { type, payload } = action
+  const { type, payload, error } = action
+
+  if ( postErrorRegexp.test( type ) ) {
+    console.error( `error` )
+    console.error( payload )
+  }
 
   switch ( type ) {
 
@@ -32,24 +36,34 @@ export default function reducer( state = initialState, action ) {
     case QUOTATION_SAVE_ONE.ERROR:
       return state.push({
         _id: new Date().valueOf(),
+        error,
         message: `error during quotation save`,
       })
 
     case CUSTOMER_SAVE_ONE.ERROR:
       return state.push({
         _id: new Date().valueOf(),
-        message: `error during customer save`,
+        error,
+        message: payload.message,
       })
 
     case ALL_POST.ERROR:
       return state.push({
         _id: new Date().valueOf(),
+        error,
         message: `something went wrong`,
       })
-
-    default:
-      return state
   }
+
+  if ( postErrorRegexp.test( type ) ) {
+    return state.push({
+      _id: new Date().valueOf(),
+      error,
+      message: payload.message,
+    })
+  }
+
+  return state
 }
 
 export const removeOne = params => async dispatch => {
