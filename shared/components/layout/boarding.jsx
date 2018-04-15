b14e86d101f311d521a735a7406fe04ba026bcd3
Hiswe <hiswehalya@gmail.com>
2018-04-15 13:56:29 +0700

    LAYOUT – change global background color on connection/preview

--- shared/components/layout/boarding.jsx
+++ shared/components/layout/boarding.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import './boarding.scss'
 const BASE_CLASS = `boarding`
@@ -6,6 +7,9 @@ const BASE_CLASS = `boarding`
 export default function LayoutBoarding( props ) {
   return (
     <main role="main" className={ `${BASE_CLASS}` }>
+      <Helmet>
+        <body className="dark-background" />
+      </Helmet>
       <div className={ `${BASE_CLASS}__card` }>
         { props.title && (<h2 className={ `${BASE_CLASS}__title` }>{props.title}</h2>) }
         <div  className={ `${BASE_CLASS}__content` }>
