d282ffcaeece023c32165f7ca1930f1feb0990ab
Hiswe <hiswehalya@gmail.com>
2018-04-18 22:04:06 +0700

    TABLE – fix pagination loosing ordering – close #47

--- shared/ui/table.header.js
+++ shared/ui/table.header.js
@@ -22,64 +22,32 @@ function Th( props ) {
   )
 
   return (
-    <th {...rest} onClick={ onClick } className={ COMP_CLASS }>
+    <th
+      onClick={ onClick }
+      className={ COMP_CLASS }
+      {...rest}
+    >
       { isSorted && <Icon svgId={ dir === `ASC` ? `arrow-upward` : `arrow-downward` } /> }
       { label && <FormattedMessage id={label.trim()} /> }
     </th>
   )
 }
 
-export class TableThead extends React.PureComponent {
+export function Thead( props ) {
+  const { columns, handleSort, sort, dir } = props
 
-  constructor( props ) {
-    super( props )
-
-    this.state = {}
-    this.handleSort = this.handleSort.bind( this )
-  }
-
-  static computeSortQuery( currentSorting, sort ) {
-    const isSameSort = currentSorting.sort === sort
-    if ( !isSameSort ) return { sort, dir: `ASC` }
-    const isAscDir   = currentSorting.dir === `ASC`
-    if ( isAscDir )    return { sort, dir: `DESC` }
-    return {}
-  }
-
-  handleSort( event, sort ) {
-    event.preventDefault()
-    if ( !sort ) return
-    const { handlePageSort, match } = this.props
-    if ( !handlePageSort ) return
-    const query = Thead.computeSortQuery( this.state, sort )
-    handlePageSort({
-      query,
-      ...match.params,
-    })
-    this.setState( prevState => ({
-      sort: query.sort,
-      dir:  query.dir,
-    }))
-  }
-
-  render() {
-    const { columns, match, location } = this.props
-    const { sort, dir } = this.state
-    return (
-      <thead className={ BASE_CLASS }>
-        <tr className={`${BASE_CLASS}_row`}>
-          { columns.map( (c, i) => (
-            <Th
-              key={ i }
-              column={ c }
-              isSorted={ c.sort && sort === c.sort }
-              dir={ dir }
-              onClick={ !c.sort ? null : event => this.handleSort(event, c.sort ) }
-            />)) }
-        </tr>
-      </thead>
-    )
-  }
+  return (
+    <thead className={ BASE_CLASS }>
+      <tr className={`${BASE_CLASS}_row`}>
+        { columns.map( (c, i) => (
+          <Th
+            key={ i }
+            column={ c }
+            isSorted={ c.sort && sort === c.sort }
+            dir={ dir }
+            onClick={ !c.sort ? null : event => handleSort(event, c.sort ) }
+          />)) }
+      </tr>
+    </thead>
+  )
 }
-
-export const Thead = withRouter( TableThead )
--- shared/ui/table.js
+++ shared/ui/table.js
@@ -1,7 +1,7 @@
 import   React              from 'react'
 import   classNames         from 'classnames'
-import { Link             } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
+import { Link, withRouter } from 'react-router-dom'
 
 import { Pagination } from './table.pagination'
 import { Thead } from './table.header'
@@ -39,8 +39,10 @@ export function Row( props ) {
     <TableContext.Consumer>
       { columns => (
         <tr className={`${BASE_CLASS}__body_row`}>
-          { React.Children.map( props.children, (child, index) => {
-            if ( child === null) return null
+          { React.Children.map( props.children, child => {
+            // ignore non rendered <th>
+            if ( child === null ) return null
+            // columns informations has been filtered already
             colCount += 1
             return React.cloneElement( child, {
               type: child.props.type || columns[ colCount ].type
@@ -63,43 +65,126 @@ export function EmptyLine( props ) {
   )
 }
 
-export function Table( props ) {
-  let { className, presentation, columns, handlePageSort } = props
-  columns = normalizeColumns( columns )
-  const hasFooter = props.footer != null
-  const hasMeta   = props.meta   != null
-  const COMP_CLASS = classNames(
-    BASE_CLASS,
-    className,
-    presentation ? `${BASE_CLASS}--presentation` : false,
-  )
-  return (
-    <div className={ COMP_CLASS }>
-      <TableContext.Provider value={ columns }>
-        <table cellSpacing="0" className={`${BASE_CLASS}__content`}>
-          { props.title && (
-            <caption className={`${BASE_CLASS}__title`}>
-              <FormattedMessage id={ props.title } />
-            </caption>
-          )}
-          <Thead
-            columns={ columns }
-            handlePageSort={ handlePageSort }
-          />
-          <tbody className={`${BASE_CLASS}__body`}>
-            { props.children }
-          </tbody>
-          { hasFooter && props.footer }
-        </table>
-      </TableContext.Provider>
-      { hasMeta && (
-        <Pagination
-          meta={ props.meta }
-          handlePageSort={ handlePageSort }
-        />
-      )}
-    </div>
-  )
+export function computeSortQuery( currentSorting, sort ) {
+  const isSameSort = currentSorting.sort === sort
+  if ( !isSameSort ) return { sort, dir: `ASC` }
+  const isAscDir   = currentSorting.dir === `ASC`
+  if ( isAscDir )    return { sort, dir: `DESC` }
+  return {}
 }
+
+export class PaginatedTable extends React.PureComponent {
+
+  constructor( props ) {
+    super( props )
+
+    this.state = {
+      sort   : undefined,
+      dir    : undefined,
+    }
+    this.columns    = normalizeColumns( props.columns )
+    this.handleSort = this.handleSort.bind( this )
+    this.handlePrev = this.handlePrev.bind( this )
+    this.handleNext = this.handleNext.bind( this )
+  }
+
+  handleSort( event, sort ) {
+    event.preventDefault()
+    if ( !sort ) return
+    const { handlePageSort, match } = this.props
+    if ( !handlePageSort ) return
+    const query = computeSortQuery( this.state, sort )
+
+    handlePageSort({
+      query,
+      ...match.params,
+    })
+    this.setState( prevState => ({
+      sort: query.sort,
+      dir:  query.dir,
+    }))
+  }
+
+  // always pass URL params to redux actions
+  // • needed for example by /customers/quotations
+  handlePrev( event ) {
+    event.preventDefault()
+    const { meta, handlePageSort, match } = this.props
+    if ( !handlePageSort )    return
+    if ( !meta.previousPage ) return
+    handlePageSort({
+      query: {
+        page: meta.previousPage,
+        ...this.state,
+      },
+      ...match.params
+    })
+  }
+
+  handleNext( event ) {
+    event.preventDefault()
+    const { meta, handlePageSort, match } = this.props
+    if ( !handlePageSort ) return
+    if ( !meta.nextPage )  return
+    handlePageSort({
+      query: {
+        page: meta.nextPage,
+        ...this.state,
+      },
+      ...match.params
+    })
+  }
+
+  render() {
+    const { props, state } = this
+    const {
+      className,
+      presentation,
+      handlePageSort
+    } = props
+    const hasFooter = props.footer != null
+    const hasMeta   = props.meta   != null
+    const COMP_CLASS = classNames(
+      BASE_CLASS,
+      className,
+      presentation ? `${BASE_CLASS}--presentation` : false,
+    )
+    return (
+      <TableContext.Provider value={ this.columns }>
+        <div className={ COMP_CLASS }>
+          <table cellSpacing="0" className={`${BASE_CLASS}__content`}>
+            { props.title && (
+              <caption className={`${BASE_CLASS}__title`}>
+                <FormattedMessage id={ props.title } />
+              </caption>
+            )}
+            <Thead
+              columns={ this.columns }
+              handleSort={ this.handleSort }
+              sort={ state.sort }
+              dir={ state.dir }
+            />
+              <tbody className={`${BASE_CLASS}__body`}>
+                { props.children }
+              </tbody>
+
+            { hasFooter && props.footer }
+          </table>
+          { hasMeta && (
+            <Pagination
+              meta={ props.meta }
+              handlePrev={ this.handlePrev }
+              handleNext={ this.handleNext }
+            />
+          )}
+        </div>
+      </TableContext.Provider>
+    )
+  }
+}
+
+// we need to have access to the router
+// • the redux actions may need to access to route params
+export const Table = withRouter( PaginatedTable )
 
 export default Table
