e63218be7658a1975bf792d1db584414eff27c64
Hiswe <hiswehalya@gmail.com>
2018-04-29 17:28:01 +0700

    TABLES – fix empty pagination I18N error – close #65

--- packages/app/shared/ui-table/pagination.js
+++ packages/app/shared/ui-table/pagination.js
@@ -1,11 +1,17 @@
 import   React              from 'react'
+import   crio               from 'crio'
 import   classNames         from 'classnames'
 import { FormattedMessage } from 'react-intl'
 
 import { Icon } from '../ui/svg-icons'
 
-const BASE_CLASS   = `table__pagination`
-const ACTION_CLASS = `${BASE_CLASS}_action`
+const BASE_CLASS      = `table__pagination`
+const ACTION_CLASS    = `${BASE_CLASS}_action`
+const emptyPagination = crio({
+  start:  `-`,
+  end:    `-`,
+  total:  `-`,
+})
 
 export function Pagination( props ) {
   const { meta, handlePrev, handleNext } = props
@@ -19,13 +25,16 @@ export function Pagination( props ) {
     `${ACTION_CLASS}--next`,
     meta.nextPage ? false :  `${ACTION_CLASS}--disabled`,
   )
+  // be sure to feed the a pagination to I18N
+  // https://github.com/Hiswe/a-count/issues/65
+  const pagination  = emptyPagination.merge(null ,meta)
   return (
     <footer className={`${BASE_CLASS}`}>
       <div className={`${BASE_CLASS}_rows`}>
         <FormattedMessage id="table.rows-per-page" />
         <span className={`${BASE_CLASS}_rows-per-page`}>{ meta.limit }</span>
       </div>
-      <FormattedMessage id="table.pagination" values={ meta } />
+      <FormattedMessage id="table.pagination" values={ pagination } />
       <div className={`${BASE_CLASS}_actions`}>
         <div
           onClick={ handlePrev }
