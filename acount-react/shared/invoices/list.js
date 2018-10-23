a6a7d8bbb9319edcded665ef9ccc12b4d442b201
Hiswe <hiswehalya@gmail.com>
2018-10-23 19:00:45 +0700

    REACT – fix building

--- acount-react/shared/invoices/list.js
+++ acount-react/shared/invoices/list.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Link               } from 'react-router-dom'
-import { connect            } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import * as invoices        from '../redux-ducks/invoices'
+import * as invoices from '../redux-ducks/invoices'
 import { Table, Row, Cell } from '../ui-table'
-import { Amount }     from '../ui/format'
-import { Progress       }   from '../ui/progress'
-import { ArchiveInvoice }   from './buttons'
+import { Amount } from '../ui/format'
+import { Progress } from '../ui/progress'
+import { ArchiveInvoice } from './buttons'
 
-function InvoiceRow( props ) {
+function InvoiceRow(props) {
   const { invoice } = props
-  const isArchived  = invoice.get(`archivedAt`)
-  const invoiceUrl  = `${ isArchived ? `/archives` : `` }/invoices/${invoice.id}`
+  const isArchived = invoice.get(`archivedAt`)
+  const invoiceUrl = `${isArchived ? `/archives` : ``}/invoices/${invoice.id}`
   return (
     <Row>
       <Cell>
-        <Link to={ invoiceUrl }>{ invoice.get( `reference` ) }</Link>
+        <Link to={invoiceUrl}>{invoice.get(`reference`)}</Link>
       </Cell>
       <Cell>
-        <Link to={ invoiceUrl }>{ invoice.get( `name` ) }</Link>
+        <Link to={invoiceUrl}>{invoice.get(`name`)}</Link>
       </Cell>
       <Cell>
         <Link to={`/customers/${invoice.customerId}`}>
-          {invoice.get( `customer.name` )}
+          {invoice.get(`customer.name`)}
         </Link>
       </Cell>
       <Cell>
@@ -32,83 +32,98 @@ function InvoiceRow( props ) {
         </Link>
       </Cell>
       <Cell>
-        <Amount
-          value={invoice.get(`total`)}
-        />
+        <Amount value={invoice.get(`total`)} />
       </Cell>
       <Cell>
         <Progress
           tableLayout
-          value={ invoice.get(`totalPaid`) }
-          max={ invoice.get(`total`) }
+          value={invoice.get(`totalPaid`)}
+          max={invoice.get(`total`)}
         />
       </Cell>
       <Cell>
-        <ArchiveInvoice icon linkAlike invoice={ invoice } />
+        <ArchiveInvoice icon linkAlike invoice={invoice} />
       </Cell>
     </Row>
   )
 }
 
 const invoiceColumns = [
-  {id: `id`       , label: `table.header.id`        , sort: `index`           , type: `id`       },
-  {id: `name`     , label: `table.header.name`      , sort: `name`            , type: `text`     },
-  {id: `customer` , label: `table.header.customer`  , sort: `customer.name`   , type: `customer` },
-  {id: `quotation`, label: `table.header.quotation` , sort: `quotation.index` , type: `id`       },
-  {id: `amount`   , label: `table.amount`           , sort: `total`           , type: `amount`   },
-  {id: `paid`     , label: `table.amount.paid`      , sort: `totalPaid`       , type: `progress` },
-  {id: `archive`  , label: false                    , sort: false             , type: `action`   },
+  { id: `id`, label: `table.header.id`, sort: `index`, type: `id` },
+  { id: `name`, label: `table.header.name`, sort: `name`, type: `text` },
+  {
+    id: `customer`,
+    label: `table.header.customer`,
+    sort: `customer.name`,
+    type: `customer`,
+  },
+  {
+    id: `quotation`,
+    label: `table.header.quotation`,
+    sort: `quotation.index`,
+    type: `id`,
+  },
+  { id: `amount`, label: `table.amount`, sort: `total`, type: `amount` },
+  {
+    id: `paid`,
+    label: `table.amount.paid`,
+    sort: `totalPaid`,
+    type: `progress`,
+  },
+  { id: `archive`, label: false, sort: false, type: `action` },
 ]
 
-function InvoiceList( props ) {
-  const {
-    invoices = [],
-    ...others
-  } = props
+function InvoiceList(props) {
+  const { invoices = [], ...others } = props
   return (
-    <Table
-      presentation
-      columns={ invoiceColumns }
-      { ...others }
-    >
-    { invoices.map( invoice => (
-      <InvoiceRow
-        key={ invoice.id }
-        invoice={ invoice }
-      />
-    ))}
+    <Table presentation columns={invoiceColumns} {...others}>
+      {invoices.map(invoice => (
+        <InvoiceRow key={invoice.id} invoice={invoice} />
+      ))}
     </Table>
   )
 }
 
 export const ActiveInvoices = connect(
   state => ({
-    invoices: state.invoices.get(`active`     ),
-    meta    : state.invoices.get(`meta.active`),
+    invoices: state.invoices.get(`active`),
+    meta: state.invoices.get(`meta.active`),
   }),
-  dispatch => ( bindActionCreators({
-    handlePageSort: invoices.getAll,
-  }, dispatch ))
-)( InvoiceList )
+  dispatch =>
+    bindActionCreators(
+      {
+        handlePageSort: invoices.listActive,
+      },
+      dispatch,
+    ),
+)(InvoiceList)
 
 export const ArchivedInvoices = connect(
   state => ({
-    invoices    : state.invoices.get(`archived` ),
-    meta        : state.invoices.get(`meta.archived`),
-    hideColumns : [`archive`],
+    invoices: state.invoices.get(`archived`),
+    meta: state.invoices.get(`meta.archived`),
+    hideColumns: [`archive`],
   }),
-  dispatch => ( bindActionCreators({
-    handlePageSort: invoices.listArchived,
-  }, dispatch ))
-)( InvoiceList )
+  dispatch =>
+    bindActionCreators(
+      {
+        handlePageSort: invoices.listArchived,
+      },
+      dispatch,
+    ),
+)(InvoiceList)
 
 export const CustomerInvoices = connect(
   state => ({
-    invoices     : state.invoices.get(`active` )    ,
-    meta         : state.invoices.get(`meta.active`),
-    hideColumns  : [`customer`],
+    invoices: state.invoices.get(`active`),
+    meta: state.invoices.get(`meta.active`),
+    hideColumns: [`customer`],
   }),
-  dispatch => ( bindActionCreators({
-    handlePageSort: invoices.listForCustomer,
-  }, dispatch ))
-)( InvoiceList )
+  dispatch =>
+    bindActionCreators(
+      {
+        handlePageSort: invoices.listForCustomer,
+      },
+      dispatch,
+    ),
+)(InvoiceList)
--- acount-react/webpack.config.js
+++ acount-react/webpack.config.js
@@ -1,21 +1,22 @@
 `use strict`
 
-const path    = require( `path`    )
-const chalk   = require( `chalk`   )
-const webpack = require( `webpack` )
-const args    = require( `yargs`   ).argv
-const nodeExternals        = require( `webpack-node-externals`      )
-const ExtractTextPlugin    = require( `extract-text-webpack-plugin` )
-const BundleAnalyzerPlugin = require( `webpack-bundle-analyzer` ).BundleAnalyzerPlugin
+const path = require(`path`)
+const chalk = require(`chalk`)
+const webpack = require(`webpack`)
+const args = require(`yargs`).argv
+const nodeExternals = require(`webpack-node-externals`)
+const ExtractTextPlugin = require(`extract-text-webpack-plugin`)
+const BundleAnalyzerPlugin = require(`webpack-bundle-analyzer`)
+  .BundleAnalyzerPlugin
 
-const isProd  = ( args.env && args.env.production )
-const isDev   = !isProd
-const env     = isProd ? `production` : `development`
+const isProd = args.env && args.env.production
+const isDev = !isProd
+const env = isProd ? `production` : `development`
 
-console.log( `build with environment`, chalk.magenta(env) )
+console.log(`build with environment`, chalk.magenta(env))
 
 process.on('uncaughtException', error => {
-  console.error( error.stack )
+  console.error(error.stack)
 })
 
 ////////
@@ -23,10 +24,10 @@ process.on('uncaughtException', error => {
 ////////
 
 const server = {
-  entry:  path.join( __dirname, `./server/index.js` ),
+  entry: path.join(__dirname, `./server/index.js`),
   output: {
     filename: `application-server.js`,
-    path:     path.resolve( __dirname, `./` ),
+    path: path.resolve(__dirname, `./`),
   },
   // this will prevent bundling node native modules
   target: `node`,
@@ -41,41 +42,48 @@ const server = {
     new webpack.BannerPlugin({
       banner: 'require("source-map-support").install();',
       raw: true,
-      entryOnly: false
+      entryOnly: false,
     }),
     new webpack.NormalModuleReplacementPlugin(
       /isomorphic-config$/,
-      path.join(__dirname, `./shared/isomorphic-config-server`)
+      path.join(__dirname, `./shared/isomorphic-config-server`),
     ),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify( env ),
+      'process.env.NODE_ENV': JSON.stringify(env),
     }),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
   // define mode as `none` because we don't want minifying in production
-  mode:   `none`,
+  mode: `none`,
   // prevent bundling node_modules on server
   // • just ignore them :)
   // • https://www.npmjs.com/package/webpack-node-externals#quick-usage
+  // since we're using yarn workspaces there is some issues with nodeExternals
+  // https://github.com/liady/webpack-node-externals/issues/39#issuecomment-356647854
   externals: [
     nodeExternals(),
-  ],
-  devtool:    `inline-source-map`,
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, '../node_modules')
+    }),
+],
+  devtool: `inline-source-map`,
   module: {
-    rules: [{
-      test: /\.js$/,
-      include: [
-        path.resolve( __dirname, `server` ),
-        path.resolve( __dirname, `shared` ),
-      ],
-      use: {
-        loader: `babel-loader`,
-        options: {
-          babelrc: true,
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, `server`),
+          path.resolve(__dirname, `shared`),
+        ],
+        use: {
+          loader: `babel-loader`,
+          options: {
+            babelrc: true,
+          },
         },
       },
-    }]
-  }
+    ],
+  },
 }
 
 ////////
@@ -84,102 +92,101 @@ const server = {
 
 const client = {
   target: `web`,
-  mode:   env,
-  entry:  path.join( __dirname, `./client/index.js`),
+  mode: env,
+  entry: path.join(__dirname, `./client/index.js`),
   output: {
-    filename:      `application-client.js`,
+    filename: `application-client.js`,
     chunkFilename: `[name].application-client.js`,
-    path:          path.resolve( __dirname, `./server/public` ),
+    path: path.resolve(__dirname, `./server/public`),
   },
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode:   `static`,
-      openAnalyzer:   false,
-      defaultSizes:   `stat`,
+      analyzerMode: `static`,
+      openAnalyzer: false,
+      defaultSizes: `stat`,
       reportFilename: `webpack-report.html`,
     }),
-    new webpack.ContextReplacementPlugin(
-      /moment[\/\\]locale$/,
-      /fr\.js/,
-    ),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr\.js/),
     new webpack.DefinePlugin({
-      "process.env": {
-        BROWSER:  JSON.stringify( true ),
-        NODE_ENV: JSON.stringify( env ),
-        IS_DEV:   JSON.stringify( isDev ),
-        IS_PROD:  JSON.stringify( isProd ),
+      'process.env': {
+        BROWSER: JSON.stringify(true),
+        NODE_ENV: JSON.stringify(env),
+        IS_DEV: JSON.stringify(isDev),
+        IS_PROD: JSON.stringify(isProd),
       },
     }),
     new webpack.NormalModuleReplacementPlugin(
       /isomorphic-config$/,
-      path.join(__dirname, `./shared/isomorphic-config-browser`)
+      path.join(__dirname, `./shared/isomorphic-config-browser`),
     ),
-    new ExtractTextPlugin( `application-client.css` ),
+    new ExtractTextPlugin(`application-client.css`),
   ],
-  devtool:  isDev ? `inline-source-map` : false,
+  devtool: isDev ? `inline-source-map` : false,
   // https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendor: {
-          chunks:   `all`,
-          test:     path.resolve(__dirname, `node_modules`),
-          name:     `vendor`,
-          enforce:  true,
-        }
-      }
-    }
-  },
-  module: {
-    rules: [{
-      test:     /\.js$/,
-      include: [
-        path.resolve( __dirname, `client` ),
-        path.resolve( __dirname, `shared` ),
-      ],
-      use: {
-        loader: `babel-loader`,
-        options: {
-          babelrc: false,
-          presets: [
-            `@babel/preset-env`,
-            `@babel/preset-react`,
-          ],
-          plugins: [
-            `@babel/transform-runtime`,
-            `@babel/plugin-proposal-object-rest-spread`,
-            `@babel/plugin-proposal-class-properties`,
-          ],
+          chunks: `all`,
+          test: path.resolve(__dirname, `node_modules`),
+          name: `vendor`,
+          enforce: true,
         },
       },
-    }, {
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: `style-loader`,
-        use: [{
-          loader: `css-loader`,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, `client`),
+          path.resolve(__dirname, `shared`),
+        ],
+        use: {
+          loader: `babel-loader`,
           options: {
-            sourceMap: isDev,
-            url:       false,
-            minimize:  isProd,
+            babelrc: false,
+            presets: [`@babel/preset-env`, `@babel/preset-react`],
+            plugins: [
+              `@babel/transform-runtime`,
+              `@babel/plugin-proposal-object-rest-spread`,
+              `@babel/plugin-proposal-class-properties`,
+            ],
           },
-        }, {
-          loader: `postcss-loader`,
-          options: {
-            ident: 'postcss',
-            sourceMap: isDev,
-            plugins: loader => [
-              require( `autoprefixer` )(),
-            ]
-          }
-        }, {
-          loader: `sass-loader`,
-          options: {
-            sourceMap: isDev,
-          },
-        }]
-      }),
-    }],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: `style-loader`,
+          use: [
+            {
+              loader: `css-loader`,
+              options: {
+                sourceMap: isDev,
+                url: false,
+                minimize: isProd,
+              },
+            },
+            {
+              loader: `postcss-loader`,
+              options: {
+                ident: 'postcss',
+                sourceMap: isDev,
+                plugins: loader => [require(`autoprefixer`)()],
+              },
+            },
+            {
+              loader: `sass-loader`,
+              options: {
+                sourceMap: isDev,
+              },
+            },
+          ],
+        }),
+      },
+    ],
   },
 }
 
