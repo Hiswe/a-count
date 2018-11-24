30743fcbfc7fe9dfcfc8bf4c0b548567859a2295
Hiswe <hiswehalya@gmail.com>
2018-11-24 19:48:59 +0700

    HELPERS – build

--- acount-helpers/lib/src/compute-quotation.d.ts
+++ acount-helpers/lib/src/compute-quotation.d.ts
@@ -1,20 +1,24 @@
-import { Quotation } from '@acount/types';
+import { Quotation, Product } from '@acount/types';
 export interface Step {
     key: string;
     label: string;
     value?: any;
 }
 export declare type Steps = Step[];
+export interface DisplayProduct extends Product {
+    total?: number;
+}
 export interface DisplayQuotation extends Quotation {
     steps: Steps;
+    products: DisplayProduct[];
 }
 export declare function cloneQuotation(quotation: Quotation): Quotation;
 export declare function steps(quotation: Quotation): DisplayQuotation;
-export declare function removeDefaultProducts(quotation: Quotation): Quotation;
-export declare function recomputeTotals(quotation: Quotation): Quotation;
-export declare function addEmptyLine(quotation: Quotation): Quotation;
-export declare function ensureProductId(quotation: Quotation): Quotation;
-export declare function computeProductsTotal(quotation: Quotation): Quotation;
-export declare function setProductsFormPath(quotation: Quotation): Quotation;
-export declare const products: (a1: Quotation) => Quotation;
-export declare const all: (a1: Quotation) => Quotation;
+export declare function removeDefaultProducts(quotation: DisplayQuotation): DisplayQuotation;
+export declare function recomputeTotals(quotation: DisplayQuotation): DisplayQuotation;
+export declare function addEmptyLine(quotation: DisplayQuotation): DisplayQuotation;
+export declare function ensureProductId(quotation: DisplayQuotation): DisplayQuotation;
+export declare function computeProductsTotal(quotation: DisplayQuotation): DisplayQuotation;
+export declare function setProductsFormPath(quotation: DisplayQuotation): DisplayQuotation;
+export declare const products: (a1: Quotation) => DisplayQuotation;
+export declare const all: (a1: Quotation) => DisplayQuotation;
