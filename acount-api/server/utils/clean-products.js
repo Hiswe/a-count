6c2c44e3e40b8dbc05dac05d8062643f1d1d6ad6
Hiswe <hiswehalya@gmail.com>
2018-11-25 12:50:26 +0700

    API – products - begin to handle boolean checked

--- acount-api/server/utils/clean-products.js
+++ acount-api/server/utils/clean-products.js
@@ -1,22 +1,22 @@
 'use strict'
 
-const compute     = require( './compute-products'         )
-const filterArray = require( './filter-array-with-object' )
+const compute = require('./compute-products')
+const filterArray = require('./filter-array-with-object')
 
 module.exports = cleanProducts
 
-function getDefaultProduct( productConfig ) {
+function getDefaultProduct(productConfig) {
   const { description, quantity, price } = productConfig
   return { description, quantity, price }
 }
 
-function cleanProducts( params ) {
+function cleanProducts(params) {
   const { productConfig, tax, products = [] } = params
-  const defaultProduct  = getDefaultProduct( productConfig )
+  const defaultProduct = getDefaultProduct(productConfig)
   const filtered = filterArray({
-    array         : products ,
-    defaultObject : defaultProduct,
-  }).map( product => {
+    array: products,
+    defaultObject: defaultProduct,
+  }).map(product => {
     // handle checkbox result
     product.checked = product.checked === `true`
     return product
@@ -24,6 +24,6 @@ function cleanProducts( params ) {
 
   return {
     filtered,
-    totals:   compute.totals( filtered, tax )
+    totals: compute.totals(filtered, tax),
   }
 }
