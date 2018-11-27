dd4cdf58db057087edc195fe093be97a28a4234f
Hiswe <hiswehalya@gmail.com>
2018-11-27 16:02:26 +0700

    NUXT – helpers - allow empty slots in filtering vue components

--- acount-nuxt/helpers/filter-vue-components.ts
+++ acount-nuxt/helpers/filter-vue-components.ts
@@ -1,8 +1,9 @@
 // filter a node list by its tag name
-//  • list should be called and rendered un `render` function
+//  • list should be called and rendered in a `render` function
 //    or it will loose all informations
 //  • don't know why but Type VNode doesn't show vnode.componentOptions.tag
-export default (tagName: string) => (vnodes: any[]): any[] => {
+export default (tagName: string) => (vnodes?: any[]): null | any[] => {
+  if (!vnodes) return null
   const filteredNodes: any[] = []
   for (let vnode of vnodes) {
     const { tag } = vnode.componentOptions
