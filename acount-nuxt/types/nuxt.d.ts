c317a4706d4476972f7ff333953c5c833c02b310
Hiswe <hiswehalya@gmail.com>
2018-11-27 15:40:44 +0700

    NUXT – clean uneeded types

--- acount-nuxt/types/nuxt.d.ts
+++ acount-nuxt/types/nuxt.d.ts
@@ -5,6 +5,7 @@ import { Store } from 'vuex'
 import { Dictionary } from 'vue-router/types/router'
 import { CookieSerializeOptions } from 'cookie'
 import VueI18n from 'vue-i18n'
+import { NuxtCookies } from 'cookie-universal-nuxt'
 
 import { AcountMeta } from './acount'
 
@@ -52,40 +53,6 @@ export interface NuxtAxios {
   onResponseError: NuxtAxiosErrorCallback
   onResponse: NuxtAxiosResponseCallback
   onError: NuxtAxiosErrorCallback
-}
-
-//////
-// COOKIES
-//////
-
-type NuxtCookieValue = any
-
-interface NuxtCookieGetOptions {
-  fromRes?: boolean
-  parseJSON?: boolean
-}
-
-interface NuxtCookieRemoveOptions {
-  path: string
-}
-
-interface NuxtCookieGetParams {
-  name: string
-  value: any
-  opts?: CookieSerializeOptions
-}
-
-export interface NuxtCookies {
-  set: (
-    name: string,
-    value: string | Object,
-    opts?: CookieSerializeOptions,
-  ) => void
-  setAll: (cookieArray: NuxtCookieGetParams[]) => void
-  get: (name: string, opts?: NuxtCookieGetOptions) => NuxtCookieValue
-  getAll: (opts?: NuxtCookieGetOptions) => NuxtCookieValue[]
-  remove: (name: string, opts?: NuxtCookieRemoveOptions) => void
-  removeAll: () => void
 }
 
 //////
